ce22007bf1e54ed9f31706ac655ad57e6214d0b3
sfK <sfk.sjm@gmail.com>
2023-10-10 12:29:30 +0900

    make card component

--- Section 6/Hike/Hike/Component/CardView.swift
+++ Section 6/Hike/Hike/Component/CardView.swift
@@ -9,15 +9,22 @@ import SwiftUI
 
 struct CardView: View {
     var body: some View {
+        
+        // MARK: - CARD
+        
         ZStack {
-            Circle()
-                .fill(LinearGradient(colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")], startPoint: .topLeading, endPoint: .bottomLeading))
-                .frame(width: 256, height: 256)
-            
-            Image("image-1")
-                .resizable()
-                .scaledToFit()
-        }
+            CustomBackgroundView()
+            ZStack {
+                Circle()
+                    .fill(LinearGradient(colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")], startPoint: .topLeading, endPoint: .bottomLeading))
+                    .frame(width: 256, height: 256)
+                
+                Image("image-1")
+                    .resizable()
+                    .scaledToFit()
+            }
+        } //: CARD
+        .frame(width: 320, height: 570)
     }
 }
 
---
+++ Section 6/Hike/Hike/Utility/ColorExtension.swift
@@ -0,0 +1,23 @@
+//
+//  ColorExtension.swift
+//  Hike
+//
+//  Created by 성준모 on 10/10/23.
+//
+
+import Foundation
+import SwiftUI
+
+extension Color{
+    static let customGreenLight = Color("ColorGreenLight")
+    static let customGreenMedium = Color("ColorGreenMedium")
+    static let customGreenDark = Color("ColorGreenDark")
+    
+    static let customGrayLight = Color("ColorGrayLight")
+    static let customGrayMedium = Color("ColorGrayMedium")
+
+    static let customIndigoMedium = Color("ColorIndigoMedium")
+    static let customSalmonLight = Color("ColorSalmonLight")
+
+    
+}
