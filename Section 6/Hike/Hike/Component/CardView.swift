018d556ecfbc5cd3a48ae8697519559ea809ff6f
sfK <sfk.sjm@gmail.com>
2023-10-11 11:20:47 +0900

    create gradientButtonStyle, card animation

--- Section 6/Hike/Hike/Component/CardView.swift
+++ Section 6/Hike/Hike/Component/CardView.swift
@@ -8,6 +8,25 @@
 import SwiftUI
 
 struct CardView: View {
+    // MARK: - PROPERTIES
+    @State private var imageNumber:Int = 1
+    @State private var randomNumber:Int = 1
+    
+    // MARK: - FUNCTIONS
+    
+    func randomImage(){
+        print("--- BUTTON WAS PRESSED--")
+        print("old number is \(imageNumber)")
+        repeat{
+            randomNumber = Int.random(in: 1...5)
+            print("random number Generated \(randomNumber)")
+        } while randomNumber == imageNumber
+        
+        imageNumber = randomNumber
+        print("new number is \(imageNumber) ")
+        print("---Function is End ---\n")
+    }
+    
     var body: some View {
         
         // MARK: - CARD
@@ -49,12 +68,37 @@ struct CardView: View {
                         .fill(LinearGradient(colors: [Color("ColorIndigoMedium"), Color("ColorSalmonLight")], startPoint: .topLeading, endPoint: .bottomLeading))
                         .frame(width: 256, height: 256)
                     
-                    Image("image-1")
+                    Image("image-\(imageNumber)")
                         .resizable()
                         .scaledToFit()
+                        .animation(.default, value: imageNumber)
                 }
                 
                 // MARK: - FOOTER
+                
+                Button(action: {
+                    // ACTION: generate a random number
+                    randomImage()
+                },
+                       label: {
+                    Text("Explore More")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(
+                            LinearGradient(
+                                colors: [
+                                    .customGreenLight,
+                                    .customGreenMedium
+                                ],
+                                startPoint: .top,
+                                endPoint: .bottom)
+                        )
+                        .shadow(color: .black.opacity(0.25),
+                                radius: 0.25, x:1 ,y: 2)
+                }
+                )
+                .buttonStyle(GradientButton())
+                
             } //: VSTACK
         } //: CARD
         .frame(width: 320, height: 570)
