8412a7e620c0a9245e2848079a455dbdc7fd55cb
sfK <sfk.sjm@gmail.com>
2023-10-16 11:16:14 +0900

    test

--- Section 8/Pinch/Pinch/Screen/ContentView.swift
+++ Section 8/Pinch/Pinch/Screen/ContentView.swift
@@ -27,7 +27,7 @@ struct ContentView: View {
                     .padding()
                     .shadow(color: .black.opacity(0.2), radius: 12, x: 2, y: 2)
                     .opacity(isAnimating ? 1 : 0)
-                    // 화면배율
+                    // 화면배율조정
                     .scaleEffect(imageScale)
                 
                 // MARK: - 1. TAP GESTURE
