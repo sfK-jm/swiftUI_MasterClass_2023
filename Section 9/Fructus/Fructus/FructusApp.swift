c9d915efa90e94e2e8f229237c52943c7e5cc588
sfK <sfk.sjm@gmail.com>
2023-10-18 12:23:01 +0900

    creat section 9

---
+++ Section 9/Fructus/Fructus/FructusApp.swift
@@ -0,0 +1,17 @@
+//
+//  FructusApp.swift
+//  Fructus
+//
+//  Created by 성준모 on 10/18/23.
+//
+
+import SwiftUI
+
+@main
+struct FructusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
