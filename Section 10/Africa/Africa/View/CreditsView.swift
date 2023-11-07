af1a70c7ad1baa23fa7930b8d704449133101a0b
sfK <sfk.sjm@gmail.com>
2023-11-07 15:39:23 +0900

    add ipad, mac version

---
+++ Section 10/Africa/Africa/View/CreditsView.swift
@@ -0,0 +1,22 @@
+//
+//  CreditsView.swift
+//  Africa
+//
+//  Created by 성준모 on 11/7/23.
+//
+
+import SwiftUI
+
+struct CreditsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CreditsView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreditsView()
+            .previewLayout(.sizeThatFits)
+            .padding()
+    }
+}
